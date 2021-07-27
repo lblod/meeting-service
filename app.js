96d6c507ac76c796e91cfef585681380635e39d8
Sergey Andreev <sergey.andreev@redpencil.io>
2021-07-27 10:25:14 +0200

    set up drone

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
 import {prefixMap} from "./support/prefixes";
 import {app, query, sparqlEscapeString, sparqlEscapeUri} from "mu";
 app.delete('/:id', function( req, res ) {
-  const id=req.params.id;
-  // Delete meeting attributes 
-
-  Query=`
-  DELETE {
-
-  } 
-  WHERE {
-    GRAPH <http://mu.semte.ch/graphs/public> {
-      <http://lblod.info/templates/39c31a7e-2ba9-11e9-88cf-83ebfda837dc> a <http://mu.semte.ch/vocabularies/ext/Template>;
-      ?p ?o.
-    }
-  };
-  `;
-  
-  // Delete treatments, participants, agenda points, votes
-
-  // Keep documents
-
-  // A backend service
-
-  
-
-  debugger;
+  // hello
 });
