0a6eacbbb1c4450fec089ec59383db29906288cd
Sergey Andreev <sergey.andreev@redpencil.io>
2021-07-27 11:11:25 +0200

    added license and readme

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-import {prefixMap} from "./support/prefixes";
-import {app, query, sparqlEscapeString, sparqlEscapeUri} from "mu";
+import {app} from "mu";
 app.get('/', function( req, res ) {
   res.send('hello world')
-});
-app.delete('/:id', function( req, res ) {
-  // hello
 });
