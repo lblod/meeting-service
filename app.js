cb25d87efae189146ac3375c8a0d36badaeac11f
Sergey Andreev <sergey.andreev@redpencil.io>
2021-07-27 10:31:26 +0200

    testing drone

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import {prefixMap} from "./support/prefixes";
 import {app, query, sparqlEscapeString, sparqlEscapeUri} from "mu";
+app.get('/', function( req, res ) {
+  res.send('hello world')
+});
 app.delete('/:id', function( req, res ) {
   // hello
 });
