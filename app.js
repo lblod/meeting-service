e6b4c06bca780ab118896d73d45d975bb5d50a62
Sergey Andreev <sergey.andreev@redpencil.io>
2021-07-29 07:25:18 +0200

    basic functionality added

--- app.js
+++ app.js
@@ -5,27 +5,45 @@ app.get('/', function( req, res ) {
 });
 
 app.delete('/:id', async function( req, res ) {
-  debugger;
   const uri="http://data.lblod.info/id/zittingen/"+req.params.id;
-  // Delete meeting attributes 
-  let deleteQuery=`
+    
+  // Keep documents
+  const updateQuery=`
+  PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+  PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+  PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
+  PREFIX dct: <http://purl.org/dc/terms/>
   DELETE {
-    ?s ?p ?o.
+    ?containerId ext:editorDocumentStatus ?statusId.
+  }
+  INSERT {
+    ?containerId ext:editorDocumentStatus <http://mu.semte.ch/application/concepts/a1974d071e6a47b69b85313ebdcef9f7>. 
   }
   WHERE {
-    ${sparqlEscapeUri(uri)} ?p ?o.
-    ?s ?p ?o.
+    ${sparqlEscapeUri(uri)} ?meetingP ?meetingO.    
+    ?meetindId ?meetingP ?meetingO.
+
+    ?meetingId besluit:behandelt ?apId.
+    ?behandelingId dct:subject ?apId. 
+    ?behandelingId ext:hasDocumentContainer ?containerId.
+    ?containerId ext:editorDocumentStatus ?statusId.
   }
   `;
-  const response=await update(deleteQuery);
-  console.log(response);
-  debugger;
-
-  // Delete treatments, intermissions, participants, agenda points, votes
+  let response=await update(updateQuery);
   
-  // Keep documents
-
-  // A backend service
+  const deleteQuery=`
+  DELETE {
+    ?meetindId ?meetingP ?meetingO.
+  }
+  WHERE {
+    ${sparqlEscapeUri(uri)} ?meetingP ?meetingO.    
+    ?meetindId ?meetingP ?meetingO.
+  }
+  `;
+  response=await update(deleteQuery);
+  
+  res.sendStatus(204);
+  //TODO: Delete treatments, intermissions, participants, agenda points, votes
 });
 
 app.use(errorHandler);
