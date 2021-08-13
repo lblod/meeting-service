0394400eef070588305e64a8a3f1d20c5b05acf1
Sergey Andreev <sergey.andreev@redpencil.io>
2021-08-13 12:01:04 +0200

    finalized single query

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function( req, res ) {
 });
 
 app.delete('/:id', async function( req, res ) {
-  debugger;
+  
   const meetingId="http://data.lblod.info/id/zittingen/"+req.params.id;
   
   // Get all needed ids
@@ -39,6 +39,7 @@ app.delete('/:id', async function( req, res ) {
     result=await query(idQuery);
   } catch (error) {
     console.log(error);
+    res.sendStatus(404);
     return;
   }  
 
@@ -66,18 +67,18 @@ app.delete('/:id', async function( req, res ) {
 
   const queryIds={behandelingIds, apIds, voteIds, intermissionIds, containerIds}
   
-  let containerUpdateQuery='';
-  let containerDeleteQuery='';
+  let updateContainerQuery='';
+  let deleteContainerQuery='';
   let deleteVotesQuery='';
   let deleteTreatmentQuery='';
   let deleteAgendaPointsQuery='';
-  let deleteIntermissionQuery=''
+  let deleteIntermissionQuery='';
   
   queryIds.containerIds.forEach((e, i)=>{
-    containerUpdateQuery+=`
+    updateContainerQuery+=`
      ${sparqlEscapeUri(e)} ext:editorDocumentStatus <http://mu.semte.ch/application/concepts/a1974d071e6a47b69b85313ebdcef9f7>.
     `;
-    containerDeleteQuery+=`
+    deleteContainerQuery+=`
      ${sparqlEscapeUri(e)} ext:editorDocumentStatus ?statusId${i}.
     `;
   });
@@ -116,7 +117,7 @@ app.delete('/:id', async function( req, res ) {
     PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
     PREFIX dct: <http://purl.org/dc/terms/>
     DELETE {
-      ${containerDeleteQuery}
+      ${deleteContainerQuery}
       ${deleteVotesQuery}
       ${deleteTreatmentQuery}
       ${deleteAgendaPointsQuery}
@@ -124,10 +125,10 @@ app.delete('/:id', async function( req, res ) {
       ${deleteMeetingQuery} 
     }
     INSERT{
-      ${containerUpdateQuery}
+      ${updateContainerQuery}
     }
     WHERE{
-      ${containerDeleteQuery}
+      ${deleteContainerQuery}
       ${deleteVotesQuery}
       ${deleteTreatmentQuery}
       ${deleteAgendaPointsQuery}
@@ -138,107 +139,12 @@ app.delete('/:id', async function( req, res ) {
   
   try {
     result=await update(updateQuery);
+    res.sendStatus(204);
   } catch (error) {
     console.log(error);
+    res.sendStatus(404);
     return;
-  }  
-    // debugger;
-    // const uri="http://data.lblod.info/id/zittingen/"+req.params.id;
-    // //Keep documents
-    // const updateDocQuery=`
-    // PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
-    // PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
-    // PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
-    // PREFIX dct: <http://purl.org/dc/terms/>
-    // DELETE {
-    //   ?containerId ext:editorDocumentStatus ?statusId.
-    // }
-    // INSERT {
-    //   ?containerId ext:editorDocumentStatus <http://mu.semte.ch/application/concepts/a1974d071e6a47b69b85313ebdcef9f7>. 
-    // }
-    // WHERE {
-    //   ${sparqlEscapeUri(uri)} besluit:behandelt ?apId.
-    //   ?behandelingId dct:subject ?apId. 
-    //   ?behandelingId ext:hasDocumentContainer ?containerId.
-    //   ?containerId ext:editorDocumentStatus ?statusId.
-    // }
-    // `;
-    // let response=await update(updateDocQuery);
-    // console.log(response);
-    
-    // //Delete votes
-    // const deleteVotesQuery=`
-    // PREFIX dct: <http://purl.org/dc/terms/>
-    // PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
-    // DELETE {
-    //   ?voteId ?voteP ?voteO.
-    // }
-    // WHERE {
-    //   ${sparqlEscapeUri(uri)} besluit:behandelt ?apId.
-    //   ?treatmentId dct:subject ?apId.
-    //   ?treatmentId besluit:heeftStemming ?voteId.
-    //   ?voteId ?voteP ?voteO.
-    // }
-    // `;
-    // response=await update(deleteVotesQuery);
-    // console.log(response);
-
-    // //Delete treatments 
-    // const deleteTreatmentQuery=`
-    // PREFIX dct: <http://purl.org/dc/terms/>
-    // PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
-    // DELETE {
-    //   ?treatmentId ?treatmentP ?treatmentO.
-    // }
-    // WHERE {
-    //   ${sparqlEscapeUri(uri)} besluit:behandelt ?apId.
-    //   ?treatmentId dct:subject ?apId.
-    //   ?treatmentId ?treatmentP ?treatmentO.
-    // }
-    // `;
-    // response=await update(deleteTreatmentQuery);
-    // console.log(response);
-    
-    // //Delete agenda points,
-    // const deleteApQuery=`
-    // PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
-    // DELETE {
-    //   ?apId ?apP ?apO.
-    // }
-    // WHERE {
-    //   ${sparqlEscapeUri(uri)} besluit:behandelt ?apId.
-    //   ?apId ?apP ?apO.
-    // }
-    // `;
-    // response=await update(deleteApQuery);
-    // console.log(response);
-
-    // //Delete intermissions
-    // const deleteIntermissionQuery=`
-    // PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
-    // DELETE {
-    //   ?intermissionId ?intermissionP ?intermissionO.
-    // }
-    // WHERE {
-    //   ${sparqlEscapeUri(uri)} ext:hasIntermission ?intermissionId.
-    //   ?intermissionId ?intermissionP ?intermissionO.
-    // }
-    // `;
-    // response=await update(deleteIntermissionQuery);
-    // console.log(response);  
-
-    // //delete meeting
-    // const deleteMeetingQuery=`
-    // DELETE {
-    //   ${sparqlEscapeUri(uri)} ?meetingP ?meetingO.
-    // }
-    // WHERE {
-    //   ${sparqlEscapeUri(uri)} ?meetingP ?meetingO.
-    // }
-    // `;
-    // response=await update(deleteMeetingQuery);
-    // console.log(response);
-    // res.sendStatus(204);
+  }
 });
 
 app.use(errorHandler);
